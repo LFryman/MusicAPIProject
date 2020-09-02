e978a3c0ddfbb544c59743d1e7a27d050ff5fd9c
lauren <lauren.a.fryman@gmail.com>
2020-09-02 14:46:15 -0400

    Round two

---
+++ MusicAPIProject/Controllers/MusicController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MusicAPIProject.Models;
+
+namespace MusicAPIProject.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly MusicDAL _musicDAL;
+        private readonly string _apiKey;
+
+        public MusicController(IConfiguration configuration)
+        {
+            _apiKey = configuration.GetSection("ApiKeys")["MusicAPIKey"];
+            _musicDAL = new MusicDAL(_apiKey);
+        }
+
+        public async Task<IActionResult> MusicIndex()
+        {
+            var album = await _musicDAL.GetArtist(); 
+            return View(album); 
+        }
+        //public IActionResult Index()
+        //{
+        //    var AlbumList = _musicDAL.ToList();
+        //    return View(AlbumList);
+        //}
+
+    }
+}
---
+++ MusicAPIProject/Models/MusicDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace MusicAPIProject.Models
+{
+    public class MusicDAL
+    {
+        private readonly string _apikey;
+
+        public MusicDAL(string apiKey)
+        {
+            _apikey = apiKey;
+        }
+
+        public HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deezerdevs-deezer.p.rapidapi.com");
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", _apikey);
+            return client;
+        }
+
+        public async Task<List<Artist>> GetArtist()
+        {
+            var client = GetClient();
+            var response = await client.GetAsync($"/search?q=eminem");
+            var album = await response.Content.ReadAsAsync<List<Artist>>();
+            return album;
+        }
+    }
+}
